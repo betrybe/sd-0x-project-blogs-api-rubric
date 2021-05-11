09e37154e11fb657ce94838a67580248a1c0fb8e
Renato Filho <renatosousafilho@gmail.com>
2021-05-11 18:21:11 -0300

    re-run evaluator

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_req, res) => {
